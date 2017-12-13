9f177f4b6a83d66ea50d212e1e7f1036f4175577
Celina Amados <celinaamados@Celinas-MacBook-Air.local>
2017-12-13 15:13:47 -0600

    first commit

--- src/main/java/Application/AppController.java
+++ src/main/java/Application/AppController.java
@@ -1,18 +1,45 @@
 package Application;
 
+import Model.DbConn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
 
 @RestController
 public class AppController {
 
     @RequestMapping("/")
     public String index() {
-        return "UZO WebAPI";
+        ResultSet rsObj = null;
+        Connection connObj = null;
+        PreparedStatement pstmtObj = null;
+        DbConn jdbcObj = new DbConn();
+        try {
+            DataSource dataSource = jdbcObj.setUpPool();
+            System.out.println(jdbcObj.printDbStatus());
+            // Performing Database Operation!
+            connObj = dataSource.getConnection();
+            System.out.println(jdbcObj.printDbStatus());
+            return (jdbcObj.printDbStatus());
+
+        } catch (Exception e) {
+            return (e.toString());
+        }
+
     }
-
-
-
 }
