2e1af04261d75f76e23f42afff509caf842390c4
Celina Amados <celinaamados@Celinas-MacBook-Air.local>
2017-12-13 17:55:14 -0600

    first commit

--- src/main/java/Application/AppController.java
+++ src/main/java/Application/AppController.java
@@ -1,6 +1,7 @@
 package Application;
 
 import Model.DbConn;
+import Model.DbManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.web.bind.annotation.*;
@@ -21,15 +22,23 @@ import javax.sql.DataSource;
 
 @RestController
 public class AppController {
+    DbManager manager= new DbManager();
 
     @RequestMapping("/")
     public String index() {
         return("Welcome to the UZO-API");
     }
 
-    @RequestMapping(value="user", method = RequestMethod.GET)
-    public @ResponseBody int getitem(@RequestParam("data") int  itemid){
-        System.out.println(itemid);
-        return itemid;
+    @RequestMapping(value="get_student_by_id", method = RequestMethod.GET)
+    public @ResponseBody int getStudentById(@RequestParam("studentid") int  studentid){
+        //manager.getStudentById(studentid);
+        System.out.println(studentid);
+        return studentid;
+    }
+
+    @RequestMapping(value = "/insert_student_by_id/", method = RequestMethod.PUT)
+    public @ResponseBody int updateStudent(@RequestParam("studentid") int  studentid){
+        System.out.println(studentid);
+        return studentid;
     }
 }
