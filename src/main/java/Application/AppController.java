8db2bd487816ade00d9ccce16032582ed07bc787
Eric Blackmon <ericblackmon38@gmail.com>
2017-12-28 09:08:26 -0500

    update creation of s3 buckets for student resumes

--- src/main/java/Application/AppController.java
+++ src/main/java/Application/AppController.java
@@ -353,6 +353,7 @@ public class AppController {
     @PostMapping(value = "/upload_student_resume")
     public String uploadFile(@RequestParam("file") MultipartFile file, int studentId){
         System.out.println(file.getOriginalFilename());
+        System.out.println(studentId);
         return s3Operations.uploadFile(studentId,file).toString();
         //return ResponseEntity.status(HttpStatus.CREATED).build();
     }
