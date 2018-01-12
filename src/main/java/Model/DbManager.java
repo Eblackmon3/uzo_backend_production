672375462305f8b14cfcce5045aa5ee55bf577ec
Eric Blackmon <ericblackmon38@gmail.com>
2018-01-11 18:01:39 -0600

    Updated error handling

--- src/main/java/Model/DbManager.java
+++ src/main/java/Model/DbManager.java
@@ -381,6 +381,9 @@ public class DbManager {
         String sql="select * from t_student_job_map where student_id =?";
         DbConn jdbcObj = new DbConn();
         try{
+            if(student.getStudent_id()==0){
+                throw new Exception("Missing Parameter");
+            }
             //Connect to the database
             DataSource dataSource = jdbcObj.setUpPool();
             System.out.println(jdbcObj.printDbStatus());
