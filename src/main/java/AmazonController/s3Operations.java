dc2b5576ea9da2c5496d26615da6d318ee91d95c
Eric Blackmon <ericblackmon38@gmail.com>
2018-02-08 09:41:20 -0500

    Restructuring S3 buckets

--- src/main/java/AmazonController/s3Operations.java
+++ src/main/java/AmazonController/s3Operations.java
@@ -59,8 +59,8 @@ public class s3Operations {
 
     //https://s3.us-east-2.amazonaws.com/uzo-s3-bucket/1/Resume
     public static String uploadStudentFile(int studentID, MultipartFile file){
-        String fileName =  studentID+"/Resume";
-        String resume_location="https://s3.us-east-2.amazonaws.com/uzo-s3-bucket/StudentFolder/"+studentID+"/Resume";
+        String fileName =  "/studentfolder/"+studentID+"/Resume";
+        String resume_location="https://s3.us-east-2.amazonaws.com/uzo-s3-bucket/studentfolder/"+studentID+"/Resume";
         File convFile;
         try {
             convFile=multipartToFile(file);
