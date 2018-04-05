1b5b2515b7b472ac4aab0011185a9656e6d6b89f
Eric Blackmon <ericblackmon38@gmail.com>
2018-04-04 20:25:29 -0400

    taking out some print statements

--- src/main/java/StripeController/StripeController.java
+++ src/main/java/StripeController/StripeController.java
@@ -40,12 +40,10 @@ public class StripeController {
             companyInfo= manager.getCompanyById(company);
             // Create a Customer:
             Map<String, Object> chargeParams = new HashMap<>();
-            System.out.println(card.getToken_id());
             chargeParams.put("source", card.getToken_id());
             chargeParams.put("email", companyInfo.get("email"));
             chargeParams.put("description", companyInfo.get("description"));
             Customer customer = Customer.create(chargeParams);
-            System.out.println(card.getToken_id());
             return customer.getId();
 
         }catch(Exception e ){
